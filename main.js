bea6d01adc9a6f54e3feedfacb0886f8eefdb860
Takahiro matsuo <miurugi.discord@gmail.com>
2021-12-21 19:45:57 +0900

    変更後のコードです。

---
+++ main.js
@@ -0,0 +1,15 @@
+while(true){
+　
+    　let result=prompt( "日本の首都は？" );
+    　　
+    　　if( result === '東京' ) {
+    
+        alert('正解です！')
+    
+    　　}
+    　　　else if(result!=='東京'){
+        
+        alert('不正解です！')
+    　
+    　}
+    }
